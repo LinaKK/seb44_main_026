31ffdedabcb5a3ac8010279f933416213b78ad95
LinaKK <lilikim0112@gmail.com>
2024-09-29 18:05:28 +0900

    ref: getProdcuts메서드에서 호출하는 메서드getProductsWithImage(getProducts, cartProductId)에서 getProductsWithImage(getProducts)로 변경

--- server/src/main/java/greenNare/product/controller/ProductController.java
+++ server/src/main/java/greenNare/product/controller/ProductController.java
@@ -47,17 +47,17 @@ public class ProductController {
 
         if(token == null) {
             // ProductService.class_existInMyCart() 매번 호출하지 않아도됨(로그인하지 않아 카트상품 없으므로 매번 호출하는것은 불필요)
-//            List<GetProductWithImageDto> responseProducts = productService.getProductsWithImage(getProducts);
-//            MultiResponseDto response = new MultiResponseDto(responseProducts, getProducts);
-//            return new ResponseEntity<>(response, HttpStatus.OK);
+            List<GetProductWithImageDto> responseProducts = productService.getProductsWithImage(getProducts);
+            MultiResponseDto response = new MultiResponseDto(responseProducts, getProducts);
+            return new ResponseEntity<>(response, HttpStatus.OK);
 
-            //한가지 service 메서드로 사용가능, 코드 추가 시 로그인하지 않은상태에서 임시 카트상품 저장가능
+            //한가지 service 메서드로 사용가능
             // List<GetProductWithImageDto>에 넣을 객체마다 매번 ProductService.class_existInMyCart() 호출하게되는데 불필요하다고 예상
             // -> ProductService.class_getProductsWithImage() 에서 빈 리스트를 구분하여 ProductService.class_existInMyCart()를 매번 호출하지 않도록함
-            List<Integer> cartProductId = new ArrayList<>();
-            List<GetProductWithImageDto> responseProductsWithCart = productService.getProductsWithImage(getProducts, cartProductId);
-            MultiResponseDto response = new MultiResponseDto(responseProductsWithCart, getProducts);
-            return new ResponseEntity<>(response, HttpStatus.OK);
+//            List<Integer> cartProductId = new ArrayList<>();
+//            List<GetProductWithImageDto> responseProductsWithCart = productService.getProductsWithImage(getProducts, cartProductId);
+//            MultiResponseDto response = new MultiResponseDto(responseProductsWithCart, getProducts);
+//            return new ResponseEntity<>(response, HttpStatus.OK);
         }
 
         //토큰있으면 카트상품리스트도 같이 전송
